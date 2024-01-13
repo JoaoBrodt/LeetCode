83082d6538701016293a6826dade19018946d3ef
João Brodt <91092208+JoaoBrodt@users.noreply.github.com>
2024-01-12 21:49:33 -0300

    Add files via upload

---
+++ 8/8.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var numIdenticalPairs = function(nums) {
+    var goodPairs = 0;
+
+    for(i=0; i<=nums.length; i++){
+        for(j=i+1; j<=nums.length; j++){
+            if(nums[i] == nums[j]){
+                goodPairs+=1;
+            }
+        }
+    }
+    return goodPairs
+    
+};
