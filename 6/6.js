f9871b2b6e9bce176ba5c658c3ced1c31b2503f0
João Brodt <91092208+JoaoBrodt@users.noreply.github.com>
2024-01-12 21:15:45 -0300

    Add files via upload

---
+++ 6/6.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var buildArray = function(nums) {
+    var ans = [];
+
+    for(i=0; i<nums.length; i++ ){
+      ans[i] = nums[nums[i]]
+    }
+
+    return ans;
+};
